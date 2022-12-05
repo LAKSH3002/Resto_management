45cd99e1b3f1a601250255588388c8ec55de7afa
LAKSH DOSHI <102138784+LAKSH3002@users.noreply.github.com>
2022-12-05 22:31:44 +0530

    Update page1_starters.java

--- page1_starters.java
+++ page1_starters.java
@@ -7,13 +7,17 @@ public class starters extends JFrame implements ActionListener
 {
     JFrame j;
     JComboBox cb;
-    JButton b,b1;
+    JButton b,b1,all,none,selective;
+    JTextField t1,t2,t3,t4,t5,t6,tf;
+    JTextArea a1;
+
+    JButton b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13;
 
     starters() {
         j = new JFrame("CREAM CENTRE");
         j.setVisible(true);
         j.setLayout(null);
-        j.setBounds(250, 90, 750, 500);
+        j.setBounds(250, 90, 800, 500);
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel p = new JPanel();
@@ -85,24 +89,149 @@ public class starters extends JFrame implements ActionListener
         j.add(l15);
         l15.setBounds(460,260,45,20);
 
-         b = new JButton("<< BACK TO HOME");
+        JLabel l16 = new JLabel("ADD ONS:");
+        j.add(l16);
+        l16.setBounds(600,20,85,20);
+
+        t1 = new JTextField();
+        t1.setBounds(600,60,100,20);
+        t1.setBackground(Color.white);
+        j.add(t1);
+        t1.setVisible(true);
+
+        t2 = new JTextField();
+        t2.setBounds(600,100,100,20);
+        t2.setBackground(Color.white);
+        j.add(t2);
+        t2.setVisible(true);
+
+        t3 = new JTextField();
+        t3.setBounds(600,140,100,20);
+        t3.setBackground(Color.white);
+        j.add(t3);
+        t3.setVisible(true);
+
+        t4 = new JTextField();
+        t4.setBounds(600,180,100,20);
+        t4.setBackground(Color.white);
+        j.add(t4);
+        t4.setVisible(true);
+
+        t5 = new JTextField();
+        t5.setBounds(600,220,100,20);
+        t5.setBackground(Color.white);
+        j.add(t5);
+        t5.setVisible(true);
+
+        t6 = new JTextField();
+        t6.setBounds(600,260,100,20);
+        t6.setBackground(Color.white);
+        j.add(t6);
+        t6.setVisible(true);
+
+        JButton b3 = new JButton("+");
+        b3.setBounds(700,60,50,20);
+        j.add(b3);
+
+        JButton b4 = new JButton("-");
+        b4.setBounds(560,60,40,20);
+        j.add(b4);
+
+        JButton b5 = new JButton("+");
+        b5.setBounds(700,100,50,20);
+        j.add(b5);
+
+        JButton b6 = new JButton("-");
+        b6.setBounds(560,100,40,20);
+        j.add(b6);
+
+        JButton b7 = new JButton("+");
+        b7.setBounds(700,140,50,20);
+        j.add(b7);
+
+        JButton b8 = new JButton("-");
+        b8.setBounds(560,140,40,20);
+        j.add(b8);
+
+        JButton b9 = new JButton("+");
+        b9.setBounds(700,180,50,20);
+        j.add(b9);
+
+        JButton b10 = new JButton("-");
+        b10.setBounds(560,180,40,20);
+        j.add(b10);
+
+        JButton b11 = new JButton("+");
+        b11.setBounds(700,220,50,20);
+        j.add(b11);
+
+        JButton b12 = new JButton("-");
+        b12.setBounds(560,220,40,20);
+        j.add(b12);
+
+        JButton b13 = new JButton("+");
+        b13.setBounds(700,260,50,20);
+        j.add(b13);
+
+        JButton b14 = new JButton("-");
+        b14.setBounds(560,260,40,20);
+        j.add(b14);
+
+        b = new JButton("<< BACK TO HOME");
         b.setBounds(210,430,180,20);
         j.add(b);
 
-         b1 = new JButton("PLACE ORDER >> ");
+        b1 = new JButton("PLACE ORDER >> ");
         j.add(b1);
-        b1.setBounds(550,430,180,20);
+        b1.setBounds(590,430,180,20);
 
         b1.addActionListener(this);
         b.addActionListener(this);
 
-        JLabel l16 = new JLabel("ADD ONS:");
-        j.add(l16);
-        l16.setBounds(600,20,55,20);
+        all = new JButton("PLACE ALL");
+        j.add(all);
+        all.setBounds(220,310,120,15);
+
+        none = new JButton("PLACE NONE");
+        j.add(none);
+        none.setBounds(350,310,120,15);
+
+        selective = new JButton("PLACED SELECTIVE");
+        j.add(selective);
+        selective.setBounds(480,310,190,15);
+
+        all.addActionListener(this);
+        none.addActionListener(this);
+        selective.addActionListener(this);
+
+        tf = new JTextField("STARTERS BILL");
+        tf.setBounds(220,330,450,80);
+        j.add(tf);
+
     }
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        int n1 = Integer.parseInt(t1.getText());
+        int n2 = Integer.parseInt(t2.getText());
+        int n3 = Integer.parseInt(t3.getText());
+        int n4 = Integer.parseInt(t4.getText());
+        int n5 = Integer.parseInt(t5.getText());
+        int n6 = Integer.parseInt(t6.getText());
+
+        if(e.getSource()== all)
+        {
+        tf.setText(String.valueOf(n1*260+n2*280+n3*300+n4*269+n5*220+n6*240));
+        }
+        if(e.getSource()== none)
+        {
+            tf.setText(String.valueOf(0));
+        }
+        if(e.getSource()== selective)
+        {
+         tf.setText(String.valueOf(0+n1*260+n2*280+n3*300+n4*269+n5*220+n6*240));
+        }
+
         if(e.getSource()==b)
         {
             home_page h = new home_page();
